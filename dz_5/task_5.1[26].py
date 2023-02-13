8d7d6fac64b351ac5a2cc69fd0fe5399359c5e84
Dmitry Poddubnov <poddubnovd@yandex.ru>
2023-02-13 22:31:44 +0300

    Solution to 5.1[26]

--- dz_5/task_5.1[26].py
+++ dz_5/task_5.1[26].py
@@ -4,3 +4,16 @@
 # <function_name>(2,1) -> 2
 # <function_name>(2,3) -> 8
 # <function_name>(2,4) -> 16
+
+
+def pow_recursive(a, b):
+    if b == 1:
+        return a
+    elif b % 2 == 0:
+        res = pow_recursive(a, b // 2)
+        return res * res
+    else: 
+        res = pow_recursive(a, b // 2)
+        return res * res * a
+a, b = int(input("Введите число: ")), int(input("Введите его степень: "))
+print("Результат возведения в степень равен:",pow_recursive(a, b))
